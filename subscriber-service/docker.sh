a56b4aefe32e60b600da76ba97a8148da62f7e5a
tariknaeem87 <49512935+tariknaeem87@users.noreply.github.com>
2019-05-30 12:01:34 +0530

    Update docker.sh

--- subscriber-service/docker.sh
+++ subscriber-service/docker.sh
@@ -3,4 +3,5 @@ port=9090
 docker stop subscriber-service
 docker rm subscriber-service
 docker rmi subscriber-service
+docker image build -t subscriber-service ~/Subscriber-service/.
 docker run -i -d -p 9090:9090 --expose 9090 --name subscriber-service -t demo/subscriber-service
